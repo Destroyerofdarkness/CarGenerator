e43bfc11bcd452459a4b77ff65449bafa72d5fbc
Destroyerofdarkness <dawidadogier@gmail.com>
2025-09-16 21:04:29 +0200

    Update

--- Public/script/script.js
+++ Public/script/script.js
@@ -1,31 +1,34 @@
 const carsActive = ["KIA", "Toyota", "Lamborgini"];
+const carImgs = ["./img/KIA.avif", "./img/toyota.png", "./img/lambo.png"];
 
- function Mycar(cars){
-    for(let i = 0;i<cars; i++){
-    const licensePlate = "DB" + Math.floor(Math.random()* (1000 - 9999) + 9999);
-    const Randomcar = Math.floor(Math.random()* carsActive.length);
-    const seats = Math.floor(Math.random()* (4- 6) + 6)
+function Mycar(cars) {
+  for (let i = 0; i < cars; i++) {
+    const licensePlate =
+      "DB" + Math.floor(Math.random() * (1000 - 9999) + 9999);
+    const Randomcar = Math.floor(Math.random() * carsActive.length);
+    const seats = Math.floor(Math.random() * (4 - 6) + 6);
     let car = carsActive[Randomcar];
-    const driven = Math.floor(Math.random() *(10000-100000) + 100000)
-    let details = "Description"
-    localStorage.setItem(`${i+1}`, details)
-    localStorage.setItem(`${i+1} Car`, car)
-    localStorage.setItem(`${i+1} License plate`, licensePlate)
-    localStorage.setItem(`${i+1} Seats`, seats)
-    localStorage.setItem(`${i+1} KM driven:`, driven)
-    let infoDiv = document.getElementById("div")
-    
-    infoDiv.innerHTML += `<ul>My car details are:
+    const driven = Math.floor(Math.random() * (10000 - 100000) + 100000);
+    let details = "Description";
+    localStorage.setItem(`${i + 1}`, details);
+    localStorage.setItem(`${i + 1} Car`, car);
+    localStorage.setItem(`${i + 1} License plate`, licensePlate);
+    localStorage.setItem(`${i + 1} Seats`, seats);
+    localStorage.setItem(`${i + 1} KM driven:`, driven);
+    let infoDiv = document.getElementById("carDiv");
+
+    infoDiv.innerHTML += `<ul>
+    <h1>My car details are:</h1>
+    <img src="${carImgs[Randomcar]}" alt="Car" style="height: auto; width:400px;">
     <li>License plate:${licensePlate}</li>
     <li>Car: ${car}</li>
     <li>Seats: ${seats}</li>
     <li>KM driven: ${driven}km</li></ul>
     `;
-    }
-    document.querySelectorAll(".ranCars").forEach(div=>
-    {
-        div.style.display = "none"
-    })
-    }
+  }
+  document.querySelectorAll(".ranCars").forEach((div) => {
+    div.style.display = "none";
+  });
+}
 
-localStorage.clear()
+localStorage.clear();
