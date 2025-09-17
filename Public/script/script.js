afe13f86fc05f035345c7a5fcebf92305795cca8
Destroyerofdarkness <dawidadogier@gmail.com>
2025-09-17 20:37:02 +0200

    Update

--- Public/script/script.js
+++ Public/script/script.js
@@ -1,13 +1,33 @@
-const carsActive = ["KIA", "Toyota", "Lamborgini"];
-const carImgs = ["./img/KIA.avif", "./img/toyota.png", "./img/lambo.png"];
+const carsActive = [
+  KIA ={
+  name: "KIA",
+  seats:5,
+  image: "./img/KIA.avif"
+}, 
+Toyota = {
+  name: "Toyota",
+  seats: 5,
+  image: "./img/toyota.png"
+},
+Lamborgini ={
+  name: "Lamborghini",
+  seats: 2,
+  image: "./img/lambo.png"
+}];
+
+const carImgs = [["./img/KIA.avif"], ["./img/toyota.png"], ["./img/lambo.png"]];
+const models = [["Kia 3","Kia2"],["Toyota 2, Toyota 2"],["Lambo 1"]]
 
 function Mycar(cars) {
+  let infoDiv = document.getElementById("carDiv");
+  
+  infoDiv.innerHTML += `<h6 id="carTitle">Your cars:</h6>`
   for (let i = 0; i < cars; i++) {
-    const licensePlate =
-      "DB" + Math.floor(Math.random() * (1000 - 9999) + 9999);
-    const Randomcar = Math.floor(Math.random() * carsActive.length);
-    const seats = Math.floor(Math.random() * (4 - 6) + 6);
-    let car = carsActive[Randomcar];
+    const licensePlate = "DB" + Math.floor(Math.random() * (1000 - 9999) + 9999);
+    const Randomcar = Math.floor(Math.random() * carsActive.length); 
+    const seats = carsActive[Randomcar].seats;
+    let car = carsActive[Randomcar].name;
+    carImg = carsActive[Randomcar].image;
     const driven = Math.floor(Math.random() * (10000 - 100000) + 100000);
     let details = "Description";
     localStorage.setItem(`${i + 1}`, details);
@@ -15,13 +35,11 @@ function Mycar(cars) {
     localStorage.setItem(`${i + 1} License plate`, licensePlate);
     localStorage.setItem(`${i + 1} Seats`, seats);
     localStorage.setItem(`${i + 1} KM driven:`, driven);
-    let infoDiv = document.getElementById("carDiv");
-
     infoDiv.innerHTML += `<ul>
     <h1>My car details are:</h1>
-    <img src="${carImgs[Randomcar]}" alt="Car" style="height: auto; width:400px;">
+    <li>You got ${car}</li>
+    <img src="${carImg}" alt="Car">
     <li>License plate:${licensePlate}</li>
-    <li>Car: ${car}</li>
     <li>Seats: ${seats}</li>
     <li>KM driven: ${driven}km</li></ul>
     `;
